ea300fea6c8586bff772560cec6c322dce0453a9
Fahim Ahmed Irfan <fahimirfan70@gmail.com>
2018-07-13 18:16:57 +0600

    first commit

---
+++ app/src/main/java/com/example/irfan/mojadar/MainCourseActivity.java
@@ -0,0 +1,13 @@
+package com.example.irfan.mojadar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_course);
+    }
+}
